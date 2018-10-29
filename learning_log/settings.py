e7797a6d52143a84d9e2608bc80d66817ae3b38f
xinglejun <xing_lejun@sina.com>
2018-10-29 19:33:32 +0800

    psycopg2-binary

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -150,6 +150,7 @@ if os.getcwd() == '/app':
 	# 静态资源配置
 	BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 	STATIC_ROOT = 'staticfiles'
+	STATIC_URL = '/static/'
 	STATICFILES_DIRS = (
 		os.path.join(BASE_DIR, 'static'),
 	)
