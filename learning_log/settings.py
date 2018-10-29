2062e387709b621c47da3cf313e4b6def35e29e1
xinglejun <xing_lejun@sina.com>
2018-10-29 19:44:44 +0800

    settings

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -151,8 +151,8 @@ if os.getcwd() == '/app':
 	BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 	STATIC_ROOT = 'staticfiles'
 	STATIC_URL = '/static/'
-	STATICFILES_DIRS = (
+	STATICFILES_DIRS = [
 		os.path.join(BASE_DIR, 'static'),
-	)
+	]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
