65239315b183a02bc70abb89b87c2207bcbe28c9
xinglejun <xing_lejun@sina.com>
2018-10-30 13:57:15 +0800

    Set DEBUG=False for Heroku.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -145,8 +145,12 @@ if os.getcwd() == '/app':
 	# 让request.is_secure()承认X-Forwarded-Proto头
 	SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-	# 支持所有的主机头 (host header)
-	ALLOWED_HOSTS = ['*']
+	# 只允许Heroku托管这个项目
+	ALLOWED_HOSTS = ['xinglejun.herokuapp.com']
+
+	DEBUG = False
+	
+	
 	# 静态资源配置
 	BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 	STATIC_ROOT = 'staticfiles'
