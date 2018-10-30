7b559abb93b2b375673000f78fa56104cde45e10
xinglejun <xing_lejun@sina.com>
2018-10-30 11:34:18 +0800

    settings

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -125,7 +125,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 # My settings
 LOGIN_URL = '/users/login/'
