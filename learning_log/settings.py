2ed11f5dd8a0e6d3c2e8aed3374d4fd358b0569c
xinglejun <xing_lejun@sina.com>
2018-10-30 13:06:54 +0800

    settings

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -150,10 +150,10 @@ if os.getcwd() == '/app':
 	# 静态资源配置
 	BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 	STATIC_ROOT = 'staticfiles'
-	#STATIC_URL = '/static/'
+	STATIC_URL = '/static/'
 	STATICFILES_DIRS = [os.path.join(BASE_DIR, '/static/')]
 	
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+	STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 
 
 
