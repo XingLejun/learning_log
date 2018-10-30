763908a47ed52cd7fdd08ac1abe6455826957622
xinglejun <xing_lejun@sina.com>
2018-10-30 11:37:58 +0800

    settings

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -125,7 +125,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 # My settings
 LOGIN_URL = '/users/login/'
@@ -150,7 +150,7 @@ if os.getcwd() == '/app':
 	# 静态资源配置
 	BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 	STATIC_ROOT = 'staticfiles'
-	STATIC_URL = '/static/'
+	#STATIC_URL = '/static/'
 	STATICFILES_DIRS = [os.path.join(BASE_DIR, '/static/')]
 	
 STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
